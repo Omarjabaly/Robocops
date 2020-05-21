41281ba32bc9552211fd483a52b6d8faf88b8e8f
Omarjabaly <60774903+Omarjabaly@users.noreply.github.com>
2020-05-21 22:47:58 +0300

    Updates

---
+++ precache-manifest.ea289dacc81498ce0a39636d98d1556e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1297c37aa198e4060332f258724bf34c",
+    "url": "/Robocops/index.html"
+  },
+  {
+    "revision": "c6adbc1e08271b019081",
+    "url": "/Robocops/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "858598cb2fd2ad631b96",
+    "url": "/Robocops/static/css/main.d281c307.chunk.css"
+  },
+  {
+    "revision": "c6adbc1e08271b019081",
+    "url": "/Robocops/static/js/2.4b8a2684.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robocops/static/js/2.4b8a2684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858598cb2fd2ad631b96",
+    "url": "/Robocops/static/js/main.dc017c8a.chunk.js"
+  },
+  {
+    "revision": "81d6b9afd35954262138",
+    "url": "/Robocops/static/js/runtime-main.56d418bc.js"
+  },
+  {
+    "revision": "603a56d0d52fe6bbfd13b08a1195b7e8",
+    "url": "/Robocops/static/media/Chopsic-K6Dp.603a56d0.ttf"
+  }
+]);
