86b948d30bbba4e20d2ea085c404b88e4da4d11e
Omarjabaly <60774903+Omarjabaly@users.noreply.github.com>
2020-10-13 21:45:19 +0300

    fixing useEffect hook

--- src/containers/App.js
+++ src/containers/App.js
@@ -38,7 +38,7 @@ function App() {
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
 		.then(users => setRobots(users))
-	});
+	},[]);
 
  
 //	onSearchChange = (event) => {
